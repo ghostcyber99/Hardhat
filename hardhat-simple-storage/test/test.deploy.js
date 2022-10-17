ab87f5b3c0c6702d282abc08b9b75dd96c5c1b09
ghostcyber99 <98751168+ghostcyber99@users.noreply.github.com>
2022-10-17 13:55:31 +0100

    writting tests for the smart contracts

---
+++ hardhat-simple-storage/test/test.deploy.js
@@ -0,0 +1,31 @@
+// testing the code locally 
+const {ethers} = require("hardhat")
+const {expect, assert } = require("chai")
+
+describe("SimpleStorage", function() {
+  let simpleStorage, simpleStorageFactory
+  beforeEach(async function() {
+      simpleStorageFactory = await ethers.getContractFactory(
+        "SimpleStorage"
+      )
+      simpleStorage = await simpleStorageFactory.deploy()
+  })
+
+  it("Should start with a favourite number of 0", async function(){
+    const currentValue = await simpleStorage.retrieve()
+    //checking to see the value of the currentvalue is equal to 0
+    const expectedValue = "0"
+    assert.equal(currentValue.toString(), expectedValue)
+  })
+  it("should update when we call store", async function(){
+    const expectedValue = "7"
+    //checking the updated value
+    const transactionResponse = await simpleStorage.store(expectedValue)
+    await transactionResponse.wait(1)
+
+    //getting the current value 
+    const currentValue = await simpleStorage.retrieve()
+    
+    assert.equal(currentValue.toString(), expectedValue)
+  })
+})
